d48d11d2c6165e966d4815711e0910b6b90adedf
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-05-25 19:17:19 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -9,26 +9,25 @@ using namespace std;
 
 class Solution{
 public:
-    int dp[1001][1001];
-    int mcm(int arr[],int i, int j){
-        if(i>=j) return 0;
-        
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        
-        int mn = INT_MAX;
-        for(int k=i;k<j;k++){
-            int tempans = mcm(arr, i,k) + mcm(arr, k+1,j) + arr[i-1]*arr[k]* arr[j];
-            
-            mn = min(mn, tempans);
-        }
-        return dp[i][j]= mn;
-    }
-    int matrixMultiplication(int N, int arr[])
+    int matrixMultiplication(int n, int arr[])
     {
         // code here
-        memset(dp,-1,sizeof(dp));
-        return mcm(arr, 1, N-1);
+        int dp[n][n];
+        for(int i=1;i<n;i++)dp[i][i]=0;
+        
+        for(int cl=2;cl<=n;cl++){
+            for(int l=1;l<=(n-cl);l++){
+                int r= l+cl-1;
+                dp[l][r] = INT_MAX;
+                
+                for(int k=l;k<r;k++){
+                    dp[l][r]= min(dp[l][r],
+                                    dp[l][k] + dp[k+1][r] + arr[l-1]*arr[k]*arr[r]);
+                }
+            }
+        }
+        
+        return dp[1][n-1];
     }
 };
 
