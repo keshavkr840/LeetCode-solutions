cd3ccac6767a4d95fddd611932dea812aa8d31b5
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-05-26 11:54:35 +0530

    Time: 10 ms (24.66%), Space: 7.4 MB (40.27%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -3,19 +3,22 @@ public:
     int longestValidParentheses(string x) {
         int n = x.size();
         // if(n==0) return 0;
-        stack<pair<char,int>> st;
+        stack<int> st;
         
         for(int i=0;i<n;i++){
             
-            if(st.empty()) st.push({x[i],i});
-            // else if(x[i]=='(') st.push({x[i],i});
-            else if(st.top().first =='(' && x[i]==')') st.pop();
-            else st.push({x[i],i});
+            if(x[i]=='(') st.push(i);
+            else{
+                if(st.empty()) st.push(i);
+                else if(x[st.top()]=='(') st.pop();
+                else st.push(i);
+
+            }
         }
         if(st.empty()) return n;
         
         while(!st.empty()){
-            int pos= st.top().second;
+            int pos= st.top();
             st.pop();
             x[pos]='0';
         }
