efe59a76ccee0a0ab71212d34c6ef82d4d756de2
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-06-02 02:01:46 +0530

    Time: 395 ms (41.82%), Space: 37.2 MB (25.29%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-    int dp[2001][2001];
     bool isPalin(string &s, int i, int j){
         while(i<j){
-            if(s[i]!=s[j]) return false;
-            i++;
-            j--;
+            if(s[i++]!=s[j--]) return false;
         }
         return true;
     }
-    int solve(string &s, int i, int j){
+    int solve(vector<vector<int>> &dp, string &s, int i, int j){
         if(i>=j) return 0;
         
         if(dp[i][j]!=-1) return dp[i][j];
@@ -17,16 +14,22 @@ public:
         if(isPalin(s,i,j)) return dp[i][j]=0;
         
         int mn = INT_MAX;
-        for(int k =i;k<j; k++){
-            if(isPalin(s,i,k)){
-                int temp = 1 + solve(s,k+1,j);
-                mn = min(mn, temp);
+        for(int k=i;k<j;k++){
+            if(isPalin( s,i,k)){
+                int temp = solve(dp, s,k+1,j)+1;
+                mn= min(temp, mn);
             }
+//             int left = solve(dp, s,i,k);
+//             int right = solve(dp, s,k+1,j);
+            
+//             mn= min(left+right+1,mn);
         }
         return dp[i][j]=mn;
+        
     }
     int minCut(string s) {
-        memset(dp,-1,sizeof(dp));
-        return solve(s,0,s.size()-1);
+        int n = s.length();
+        vector<vector<int>> dp(n+1, vector<int>(n+1,-1) );
+        return solve(dp, s, 0, n-1);
     }
 };
