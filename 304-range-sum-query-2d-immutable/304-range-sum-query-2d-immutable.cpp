a2da93dca3cd47a86d3221567dc292b831ea1002
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-06-03 20:41:51 +0530

    Time: 723 ms (27.80%), Space: 147.9 MB (79.01%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -1,32 +1,21 @@
 class NumMatrix {
+private:
+    vector<vector<int>> sum;
 public:
-    vector<vector<int>> mat;
     NumMatrix(vector<vector<int>>& matrix) {
-        for(auto v :matrix){
-            vector<int> vec;
-            vec.push_back(v.front());
-            int size=v.size();
-            
-            for(int i=1;i<size;i++){
-                vec.push_back(v[i]+vec.back());
+        int m = matrix.size();
+        int n = matrix[0].size();
+        sum = vector<vector<int>> (m+1,vector<int>(n+1,0));
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                sum[i][j]= matrix[i-1][j-1] + sum[i-1][j]+ sum[i][j-1]- sum[i-1][j-1];
             }
-            mat.push_back(vec);
         }
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        int ans=0;
-        for(int i=row1;i<=row2;i++){
-            ans+=mat[i][col2];
-        }
-        
-        if(col1>0){
-            for(int i=row1;i<=row2;i++){
-                ans-=mat[i][col1-1];
-            }
-        }
-            
-        return ans;
+        return(sum[row2+1][col2+1]-sum[row2+1][col1]-sum[row1][col2+1]+ sum[row1][col1]);
     }
 };
 
