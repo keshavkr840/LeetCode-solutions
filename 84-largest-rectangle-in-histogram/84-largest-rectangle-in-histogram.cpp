68982e714549a2de5a7e1b825b71ab576558568f
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-06-16 20:49:40 +0530

    Time: 225 ms (66.66%), Space: 74.7 MB (99.37%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,51 +1,25 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        vector<int> left, right;
-        // left[0]=-1;
-        // right[n-1]= n;
-        
-        stack<int> st;
-        
-        for(int i=0;i<n;i++){
-            if(st.empty()) left.push_back(-1);
+    int largestRectangleArea(vector<int>& height) {
+                    
+            int ret = 0;
+            height.push_back(0);
+            vector<int> index;
             
-            else if(heights[st.top()] < heights[i])
-                left.push_back(st.top());
-            else{
-                while(!st.empty() && heights[st.top()]>= heights[i])
-                    st.pop();
-                if(st.empty()) left.push_back(-1);
-                else left.push_back(st.top());
+            for(int i = 0; i < height.size(); i++)
+            {
+                while(index.size() > 0 && height[index.back()] >= height[i])
+                {
+                    int h = height[index.back()];
+                    index.pop_back();
+                    
+                    int sidx = index.size() > 0 ? index.back() : -1;
+                    if(h * (i-sidx-1) > ret)
+                        ret = h * (i-sidx-1);
+                }
+                index.push_back(i);
             }
-            st.push(i);
-        }
-        
-        while(!st.empty()) st.pop();
-        
-        
-        for(int i=n-1;i>=0;i--){
-            if(st.empty()) right.push_back(n);
             
-            else if( heights[st.top()]< heights[i])
-                right.push_back(st.top());
-            
-            else{
-                while(!st.empty() && heights[st.top()]>= heights[i])
-                    st.pop();
-                
-                if(st.empty()) right.push_back(n);
-                else right.push_back(st.top());
-            }
-            st.push(i);
-        }
-        
-        reverse(right.begin(),right.end());
-        int mx=0;
-        for(int i=0;i<n;i++){
-            mx= max(mx, (right[i]-left[i]-1)*heights[i]);
-        }
-        return mx;
+            return ret;
     }
 };
