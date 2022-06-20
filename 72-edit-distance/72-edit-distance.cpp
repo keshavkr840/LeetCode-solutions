5b1293c0e21f5827def040ee6a5191637832fa98
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-06-20 21:57:12 +0530

    Time: 30 ms (32.15%), Space: 9 MB (37.62%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minDistance(string str1, string str2) {
+        int m = str1.length(), n = str2.length();
+        vector<vector<int>> dp(m+1, vector<int>(n+1,0));
+        
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0 || j==0) dp[i][j]= max(i,j);
+                
+                else if(str1[i-1]== str2[j-1]) 
+                    dp[i][j]= dp[i-1][j-1];
+                
+                else 
+                    dp[i][j]= 1+ min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]));
+            }
+        }
+        
+        return dp[m][n];
+    }
+};
