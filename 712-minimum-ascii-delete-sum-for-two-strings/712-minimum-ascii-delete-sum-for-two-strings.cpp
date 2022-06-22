beae5573b91aa6ef1e09bce3d3049797b332440b
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-06-22 15:00:19 +0530

    Time: 30 ms (83.97%), Space: 7 MB (85.86%) - LeetHub

--- 712-minimum-ascii-delete-sum-for-two-strings/712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    int solve(string &s1, string &s2, int m , int n, int ans){
-        if(m==0){
-            for(int i=n;i>0;i--)
-                ans+=(int) s2[i-1];
-            return dp[m][n]= ans;
-        }
-        if(n==0){
-            for(int i=m;i>0;i--)
-                ans+=(int) s1[i-1];
-            return dp[m][n]= ans;
-        }
-        if(dp[m][n]!=-1) return dp[m][n];
-        
-        if(s1[m-1]==s2[n-1]) return dp[m][n]= solve(s1,s2,m-1,n-1,ans);
-        else
-            return dp[m][n]= min( (int)s1[m-1]+ solve(s1,s2,m-1,n,ans),(int)s2[n-1]+ solve(s1,s2,m,n-1,ans));
-    }
     int minimumDeleteSum(string s1, string s2) {
-        memset(dp,-1,sizeof(dp));
-        return solve(s1, s2, s1.length(), s2.length(),0);
+        int m = s1.length(), n = s2.length();
+        int dp[m+1][n+1];
+        dp[0][0]=0;
+        
+        for(int i=1;i<=m;i++) dp[i][0]= dp[i-1][0]+ (int)s1[i-1];
+        
+        for(int j=1;j<=n;j++) dp[0][j]= dp[0][j-1]+ (int)s2[j-1];
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s1[i-1]==s2[j-1])
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                    dp[i][j] = min((int)s1[i-1] + dp[i-1][j], (int)s2[j-1]+ dp[i][j-1]);
+            }
+        }
+        return dp[m][n];
     }
 };
