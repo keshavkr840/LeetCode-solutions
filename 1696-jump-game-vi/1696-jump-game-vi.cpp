895ac03b2f65471cad59fed8ed12f58c681322cb
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-07-10 01:55:07 +0530

    Time: 281 ms (61.20%), Space: 77.3 MB (99.06%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -2,24 +2,21 @@ class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
         int n = nums.size();
-        if(n==1) return nums[0];
-        
-        priority_queue<pair<int, int>> pq;
-        // vector<int> ans(n,INT_MIN);
-        int curr=0;
-        pq.push({nums[0],0});
-        
-        
-        for(int i=1;i<n;i++){
-            while(!pq.empty() && pq.top().second<i-k)
-                pq.pop();
-            curr= nums[i]+ pq.top().first;
-            while(!pq.empty() && pq.top().first<curr)
-                pq.pop();
-            pq.push({curr,i});
+        deque<int> dq;
+        for(int i=0;i<n;i++){
+            
+            while(!dq.empty() && dq.front()<i-k)
+                dq.pop_front();
+            
+            int curr= nums[i]+ (dq.empty()? 0: nums[dq.front()]);
+            
+            nums[i]= curr;
+            
+            while(!dq.empty() && nums[dq.back()]<= curr)
+                dq.pop_back();
+            
+            dq.push_back(i);
         }
-        
-        // for(auto &n: ans) cout<<n<<" ";
-        return curr;
+        return nums[n-1];
     }
 };
