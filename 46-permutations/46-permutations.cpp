deb1777ac6f76a3092f7e05ad516623ef7dd552b
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-07-11 04:11:50 +0530

    Time: 10 ms (18.74%), Space: 8.2 MB (16.93%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,28 +1,30 @@
 class Solution {
-public:
-    void solve(int pos, vector<int>& nums, set<vector<int>> &s){
-        if(pos==nums.size()){
-            s.insert(nums);
+private:
+    unordered_set<int> set;
+    vector<vector<int>> ans;
+    
+    void solve(vector<int> &nums, vector<int> & temp){
+        
+        if(temp.size()==nums.size()){ 
+            ans.push_back(temp);
             return;
         }
         
-        for(int i=pos;i<nums.size();i++){
-            swap(nums[i],nums[pos]);
-            solve(pos+1,nums,s);
-            swap(nums[i],nums[pos]);
+        for(int i=0;i<nums.size();i++){
+            if(set.find(nums[i])!=set.end()) continue;
+            
+            set.insert(nums[i]);
+            temp.push_back(nums[i]);
+            solve(nums,temp);
+            set.erase(set.find(nums[i]));
+            temp.pop_back();
         }
     }
+public:
     vector<vector<int>> permute(vector<int>& nums) {
-        set<vector<int>> s;
-        // sort(nums.begin(),nums.end());
-        solve(0,nums,s);
-
-        vector<vector<int>> ans;
-        
-        for(auto v: s){
-            ans.push_back(v);
-        }
+        // for(auto &n: nums) set.insert(n);
+        vector<int> temp;
+        solve(nums, temp);
         return ans;
-        
     }
 };
