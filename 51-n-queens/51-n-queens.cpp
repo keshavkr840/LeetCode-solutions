cb7ca23c407c26246a4e5a6648de2fdb204e5efc
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-07-13 03:28:43 +0530

    Time: 4 ms (90.36%), Space: 7.5 MB (37.33%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,41 +1,44 @@
 class Solution {
-public:
-    vector<vector<string> > sols; // 2D vector of strings to store the solutions
-    vector<vector<string>> solveNQueens(int n) {    
-	    vector<string> board(n, string(n, '.'));  // creating an empty board
-	    solve(board, 0); // calling the recursive function
-	    return sols; 
-    }
-    bool isSafe(vector<string>& board, int row, int col) {
-	    int n = size(board);
-	    for(int i = 0; i < n; i++) {
-			// checking if there is a queen in the same column
-		    if(board[i][col] == 'Q') return false; 
-		    // checking if there is a queen in the same diagonal (left to right)
-		    if(row - i >= 0 && col - i >= 0 && board[row - i][col - i] == 'Q') return false;
-		    if(row - i >= 0 && col + i <  n && board[row - i][col + i] == 'Q') return false;
-		    // No need to traverse more since the lower rows below current row attribute will always be safe.
-			/* if(row + i <  n && col - i >= 0 && board[row + i][col - i] == 'Q') return false;
-			if(row + i <  n && col + i <  n && board[row + i][col + i] == 'Q') return false; */
-	    }
-	    return true;
-    }
-    // Recursive Function (solve) - It basically tries all possible placement of queen for the current row & recurses for it's next row
-    void solve(vector<string>& board, int row) {
-	// Base condition. 
-    // We reached the last row, so we have a solution so we add it to the solution vector
-	    if(row == size(board)) { 
-		    sols.push_back(board);
-		    return;
-    	}            
-	    // Try placing a queen on each column for a given row. 
-        // Explore next row by placing Q at each valid column for the current row
-	    for(int col = 0; col < size(board); col++){
-		    if(isSafe(board, row, col)) {
-			    board[row][col] = 'Q';    // Queen placed on a valid cell
-			    solve(board, row + 1);    // Exploring next row
-			    board[row][col] = '.';    // Backtracking to get all possible solutions
-		    }
+private:
+    vector<vector<string>> ans;
+    bool fill(vector<string> &temp, int &i, int &j, int &n){
+        int x= i-1, y=j-1;
+        while(x>=0 && y>=0){
+            if(temp[x--][y--]=='Q') return false;
         }
+        
+        x= i-1, y=j;
+        while(x>=0){
+            if(temp[x--][y]=='Q') return false;
+        }
+        
+        x=i-1, y=j+1;
+        while(x>=0 && y<n){
+            if(temp[x--][y++]=='Q') return false;
+        }
+        return true;
+    }
+    
+    void solve(vector<string> &temp, int &n, int row){
+        if(row==n)
+            ans.push_back(temp);
+        
+        for(int i=0;i<n;i++){
+            if(fill(temp, row, i, n)){
+                temp[row][i]='Q';
+                solve(temp, n, row+1);
+                temp[row][i]='.';
+            }
+        }
+        
+    }
+    
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        string str="";
+        for(int i=0;i<n;i++) str+=".";
+        vector<string> temp (n,str );
+        solve(temp, n, 0);
+        return ans;
     }
 };
