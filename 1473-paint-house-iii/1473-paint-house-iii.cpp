d1200bb3c39460e6a25f643d67138eb5bae85594
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-07-15 17:14:55 +0530

    Time: 31 ms (99.40%), Space: 11.7 MB (37.84%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.cpp
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -1,59 +1,31 @@
 class Solution {
+private:
+    long long dp[101][21][101];
+    long long solve(vector<int>& houses, vector<vector<int>>& cost, int &m, int &n, int target, int house, int prev){
+        if(house==m) return(target==0)?0:INT_MAX;
+        if(target<0) return INT_MAX;
+        if((m-house)<target) return INT_MAX;
+        
+        if(dp[house][prev][target] != -1) return dp[house][prev][target];
+        
+        long long mincost = INT_MAX;
+        
+        if(houses[house]!=0) 
+            mincost = solve(houses, cost, m, n, target-(prev!=houses[house]), house+1,houses[house] );
+        else{
+            // long long 
+            for(int j=0;j<n;j++){
+                long long tempcost= solve(houses, cost, m, n,target-(prev!=j+1), house+1, j+1)+ cost[house][j];
+                mincost =min(mincost, tempcost);
+            }
+        }
+        return dp[house][prev][target]=mincost;
+    }
 public:
-    // Maximum cost possible plus 1
-    const int MAX_COST = 1000001;
-
     int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
-        // Initialize memo array
-        vector<vector<vector<int>>> memo(m, vector<vector<int>>(target + 1, vector<int>(n, MAX_COST)));
-            
-        // Initialize for house 0, neighborhoods will be 1
-        for (int color = 1; color <= n; color++) {
-            if (houses[0] == color) {
-                // If the house has same color, no cost
-                memo[0][1][color - 1] = 0;
-            } else if (!houses[0]) {
-                // If the house is not painted, assign the corresponding cost
-                memo[0][1][color - 1] = cost[0][color - 1];
-            }
-        }
-        
-        for (int house = 1; house < m; house++) {
-            for (int neighborhoods = 1; neighborhoods <= min(target, house + 1); neighborhoods++) {
-                for (int color = 1; color <= n; color++) {
-                    // If the house is already painted, and color is different
-                    if (houses[house] && color != houses[house]) {
-                        // Cannot be painted with different color
-                        continue;
-                    }
- 
-                    int currCost = MAX_COST;
-                    // Iterate over all the possible color for previous house
-                    for (int prevColor = 1; prevColor <= n; prevColor++) {
-                        if (prevColor != color) {
-                            // Decrement the neighborhood as adjacent houses has different color
-                            currCost = min(currCost, memo[house - 1][neighborhoods - 1][prevColor - 1]);
-                        } else {
-                            // Previous house has the same color, no change in neighborhood count
-                            currCost = min(currCost, memo[house - 1][neighborhoods][color - 1]);
-                        }
-                    }
-
-                    // If the house is already painted cost to paint is 0
-                    int costToPaint = houses[house] ? 0 : cost[house][color - 1];
-                    memo[house][neighborhoods][color - 1] = currCost + costToPaint;
-                }
-            }
-        }
-        
-        int minCost = MAX_COST;
-        // Find the minimum cost with m houses and target neighborhoods
-        // By comparing cost for different color for the last house
-        for (int color = 1; color <= n; color++) {
-            minCost = min(minCost, memo[m - 1][target][color - 1]);
-        }
-        
-        // Return -1 if the answer is MAX_COST as it implies no answer possible
-        return minCost == MAX_COST ? -1 : minCost;
+        memset(dp,-1,sizeof(dp));
+        long long mx = INT_MAX;
+        long long ans=solve(houses,cost,m,n, target, 0,0);
+        return (ans>= INT_MAX)?-1: ans;
     }
 };
