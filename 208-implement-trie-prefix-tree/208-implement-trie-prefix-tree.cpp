8b6aa8245c376c1ebbc0b7e272cf05bfcbf60f88
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-07-17 11:59:09 +0530

    Time: 129 ms (28.77%), Space: 44.8 MB (77.10%) - LeetHub

---
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,71 @@
+class TrieNode{
+public:
+    TrieNode* next[26];
+    bool isLast = false;
+    
+    bool containsChar(char ch){
+        if(next[ch-'a']!= nullptr) return true;
+        return false;
+    }
+    
+    TrieNode* getNode(char ch){
+        return next[ch-'a'];
+    }
+    
+    void makeNode(TrieNode* node, char ch){
+        next[ch-'a']= node;
+    }
+    
+    void setLast(){
+        isLast = true;
+    }
+};
+class Trie {
+private:
+    TrieNode * root;
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    void insert(string word) {
+        auto node = root;
+        
+        for(int i=0;i< word.size();i++){
+            if(node->containsChar(word[i])) node = node->getNode(word[i]);
+            else {
+                node->makeNode(new TrieNode(), word[i]);
+                node= node->getNode(word[i]);
+            }
+        }
+        node->setLast();
+    }
+    
+    bool search(string word) {
+        auto node =root;
+        
+        for(int i=0;i<word.length();i++){
+            if(node->containsChar(word[i])) node = node->getNode(word[i]);
+            else return false;
+        }
+        return node->isLast;
+    }
+    
+    bool startsWith(string prefix) {
+        auto node =root;
+        
+        for(int i=0;i<prefix.length();i++){
+            if(node->containsChar(prefix[i])) node = node->getNode(prefix[i]);
+            else return false;
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
