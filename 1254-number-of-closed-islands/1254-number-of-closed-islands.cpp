129e4bab03806bae1c110b5701aa569ecf8d316f
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-08-03 15:47:22 +0530

    Time: 25 ms (33.60%), Space: 9.5 MB (65.78%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,17 +1,17 @@
 class Solution {
 private:
-    bool isClosed(vector<vector<int>>& grid, vector<vector<bool>> &vis, int &m , int &n, int i, int j){
-        if((i==0 || j==0 || i==m-1 || j==n-1) && (grid[i][j]==0)) return false;
+    bool isClosed(vector<vector<int>>& grid, int i, int j){
+        if(i<0 || j<0 || i==grid.size() || j == grid[0].size())
+            return false;
         
+        if(grid[i][j]==1) return true;
         
-        vis[i][j]= true;
-        if(grid[i][j]) return true;
-        
+        grid[i][j]=1;
         bool ans= true;
-        if(!vis[i][j-1]) ans= ans& isClosed(grid, vis, m, n, i, j-1);
-        if(!vis[i][j+1]) ans= ans& isClosed(grid, vis, m, n, i, j+1);
-        if(!vis[i-1][j]) ans= ans& isClosed(grid, vis, m, n, i-1, j);
-        if(!vis[i+1][j]) ans= ans& isClosed(grid, vis, m, n, i+1, j);
+        ans &= isClosed(grid, i, j-1);
+        ans &= isClosed(grid, i, j+1);
+        ans &= isClosed(grid, i-1, j);
+        ans &= isClosed(grid, i+1, j);
         
         return ans;
     }
@@ -20,13 +20,11 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         
-        vector<vector<bool>> vis(m, vector<bool>(n,false));
-        
         int ans=0;
-        for(int i=1;i<m-1;i++){
-            for(int j=1;j<n-1;j++){
-                if(!vis[i][j] && grid[i][j]==0)
-                    ans+= isClosed(grid, vis, m, n, i, j);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==0)
+                    if(isClosed(grid, i, j)) ans++;
             }
         }
         return ans;
