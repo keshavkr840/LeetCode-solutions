4f5ec04af6cc0a163bd28e41ed5e848815761e92
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-08-04 23:14:34 +0530

    Time: 87 ms (11.73%), Space: 15.3 MB (29.78%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,36 +1,25 @@
 class Solution {
+    int dp[101][101][201];
+    bool solve(string &a, string &b, string &c, int i, int j, int k){
+        if(dp[i][j][k]!=-1) return dp[i][j][k];
+        if(i==a.size())
+            return dp[i][j][k]= b.substr(j).compare(c.substr(k))==0 ;
+        else if(j==b.size())
+            return dp[i][j][k]= (a.substr(i).compare(c.substr(k))==0);
+        
+        else if(a[i]==c[k] && b[j]==c[k])
+            return dp[i][j][k]= solve(a,b,c, i+1, j, k+1) || solve(a,b,c,i, j+1, k+1);
+        else if(a[i]==c[k])
+            return dp[i][j][k]= solve(a,b,c, i+1, j, k+1);
+        else if(b[j]==c[k])
+            return dp[i][j][k]= solve(a,b,c, i, j+1, k+1);
+        return dp[i][j][k]= false;
+    }
 public:
-    bool isInterleave(string s1, string s2, string s3) {
-        if(s1.length()+s2.length()!= s3.length()) return false;
-        int m = s1.length();
-        int n = s2.length();
+    bool isInterleave(string a, string b, string c) {
+        if(a.length()+ b.length()!=c.length()) return false;
+        memset(dp, -1, sizeof(dp));
         
-        bool dp[m+1][n+1];
-        memset(dp,false,sizeof(dp));
-        
-        for(int i=0;i<=m;i++){
-            for(int j=0;j<=n;j++){
-                if(i==0 && j==0)
-                    dp[i][j]=true;
-                
-                else if(i==0){
-                    dp[i][j]= dp[i][j-1] && (s2[j-1]==s3[j-1]);
-                }
-                else if(j==0){
-                    dp[i][j]= dp[i-1][j] && (s1[i-1]==s3[i-1]);
-                }
-                else {
-                    dp[i][j]= (dp[i-1][j] && s1[i-1]==s3[i+j-1]) || (dp[i][j-1] && s2[j-1]==s3[i+j-1]);
-                }
-            }
-        }
-        // for(int i=0;i<=m;i++){
-        //     for(int j=0;j<=m;j++){
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        
-        return dp[m][n];
+        return solve(a, b,c, 0, 0, 0);
     }
 };
