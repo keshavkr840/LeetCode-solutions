bf6f7964eee02cabe24e223fbfc33e1eb86373f4
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-08-04 23:37:39 +0530

    Time: 8 ms (47.35%), Space: 6.4 MB (71.00%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,19 +1,19 @@
 class Solution {
-    int dp[101][101][201];
+    int dp[101][101];
     bool solve(string &a, string &b, string &c, int i, int j, int k){
-        if(dp[i][j][k]!=-1) return dp[i][j][k];
+        if(dp[i][j]!=-1) return dp[i][j];
         if(i==a.size())
-            return dp[i][j][k]= b.substr(j).compare(c.substr(k))==0 ;
+            return dp[i][j]= b.substr(j).compare(c.substr(k))==0 ;
         else if(j==b.size())
-            return dp[i][j][k]= (a.substr(i).compare(c.substr(k))==0);
+            return dp[i][j]= (a.substr(i).compare(c.substr(k))==0);
         
         else if(a[i]==c[k] && b[j]==c[k])
-            return dp[i][j][k]= solve(a,b,c, i+1, j, k+1) || solve(a,b,c,i, j+1, k+1);
+            return dp[i][j]= solve(a,b,c, i+1, j, k+1) || solve(a,b,c,i, j+1, k+1);
         else if(a[i]==c[k])
-            return dp[i][j][k]= solve(a,b,c, i+1, j, k+1);
+            return dp[i][j]= solve(a,b,c, i+1, j, k+1);
         else if(b[j]==c[k])
-            return dp[i][j][k]= solve(a,b,c, i, j+1, k+1);
-        return dp[i][j][k]= false;
+            return dp[i][j]= solve(a,b,c, i, j+1, k+1);
+        return dp[i][j]= false;
     }
 public:
     bool isInterleave(string a, string b, string c) {
