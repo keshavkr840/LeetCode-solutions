91d9fa21a535296e2bff3eed1234680a1128537d
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-08-16 10:58:01 +0530

    Time: 220 ms (46.06%), Space: 41.6 MB (17.64%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -6,30 +6,36 @@ private:
         int n = grid[0].size();
         
         queue<vector<int>> q;
-        q.push({entrance[0], entrance[1],0});
+        q.push(entrance);
         
-        grid[entrance[0]][entrance[1]]='0';
+        grid[entrance[0]][entrance[1]]='+';
+        
         pair<int, int> dirs[4]=  {{0,1},{0,-1}, {1,0}, {-1,0}};
-        
+        int moves=0;
         while(!q.empty()){
-            vector<int> curr= q.front();
-            q.pop();
+            int l = q.size();
             
-            for(auto &dir: dirs){
-                
-                int x= curr[0]+ dir.first;
-                int y= curr[1]+ dir.second;
-                
-                if((x<0 || y<0 || x==m || y==n)){
-                    if(curr[0]==entrance[0] && curr[1]==entrance[1]) continue;
-                    return curr[2];
-                } 
-                else if(grid[x][y]=='.'){
-                    grid[x][y]= '+';
-                    // cout<<"ok : "<<x<<" "<<y<<" "<<grid[x][y]<<endl;
-                    q.push({x, y, 1+curr[2] });
+            for(int i=0;i<l;i++){
+                vector<int> curr= q.front();
+                q.pop();
+
+                for(auto &dir: dirs){
+
+                    int x= curr[0]+ dir.first;
+                    int y= curr[1]+ dir.second;
+
+                    if((x<0 || y<0 || x==m || y==n)){
+                        if(curr[0]==entrance[0] && curr[1]==entrance[1]) continue;
+                        return moves;
+                    } 
+                    else if(grid[x][y]=='.'){
+                        grid[x][y]= '+';
+                        // cout<<"ok : "<<x<<" "<<y<<" "<<grid[x][y]<<endl;
+                        q.push({x, y});
+                    }
                 }
             }
+            moves++;
         }
         return -1;
     }
