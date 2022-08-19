abb5b3fdbc8f6fdefbb5b3ed58e03ade3be675bc
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-08-20 03:23:57 +0530

    Time: 25 ms (90.47%), Space: 13.1 MB (88.25%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -8,43 +8,43 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+#define Node ListNode
 class Solution {
 public:
-     struct comp
-    {
-        bool operator()(ListNode* a, ListNode *b)
-        {
-            return a->val > b->val;   // min heap  
-        }
-    };
     
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size()==0) return nullptr;
+    ListNode* merge2Lists(ListNode* a, ListNode* b){
+        ListNode dummy(0);
+        ListNode* node= &dummy;
         
-        // bool comp= [&](ListNode* a, ListNode *b){
-        //     return a->val > b->val;
-        // };
-        priority_queue< ListNode*, vector<ListNode*>, comp> pq;
-        
-        for(int i=0;i<lists.size();i++){
-            if(lists[i]!=nullptr)
-                pq.push(lists[i]);
-        }
-        
-        ListNode dummy (0);
-        ListNode * node = &dummy;
-        
-        while(!pq.empty()){
-            ListNode* curr= pq.top();
-            pq.pop();
-            
-            // ListNode* t = new ListNode(curr->val);
-            node->next = curr;
+        while(a && b){
+            if(a->val <= b->val){
+                node->next =a;
+                a= a->next;
+            }
+            else{
+                node->next =b;
+                b= b->next;
+            }
             node= node->next;
-            
-            if(curr->next) pq.push(curr->next);
         }
-        
+        if(a)
+            node->next = a;
+        else
+            node->next = b;
         return dummy.next;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
+        if(lists.size()==0) return nullptr;
+        int rem =lists.size();
+        
+        while(rem>1){
+            for(int i=0;i<rem/2;i++){
+                lists[i]= merge2Lists(lists[i], lists[rem-1-i]);
+            }
+            rem= (rem+1)/2;
+        }
+            
+        return lists.front();
     }
 };
