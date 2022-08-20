4beced7799da08fdca7d8c160fa589fe29228930
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-08-21 02:03:54 +0530

    Time: 50 ms (73.23%), Space: 19 MB (66.50%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
-    static bool comp(vector<int> &a, vector<int> &b){
-        if(a[0]==b[0]) return a[1]> b[1];
-        else return a[0]<b[0];
-    }
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int sz= intervals.size();
-        if(sz==1) return intervals;
+    vector<vector<int>> merge(vector<vector<int>>& v) {
+        int n = v.size();
+        if(n==1) return v;
         
-        sort(intervals.begin(), intervals.end(), comp);
-//         for(auto &v: intervals){
-//             cout<<v[0]<<", "<<v[1]<<endl;
-//         }
-        
+        sort(v.begin(), v.end());
         vector<vector<int>> ans;
         
-        int l=0, mx= intervals[0].back();
-        
-        for(int i=1;i<sz;i++){
-            if( mx < intervals[i].front()){
-                ans.push_back({intervals[l].front(), mx});
-                mx= intervals[i].back();
-                l=i;
+        for(auto &n: v){
+            if(ans.empty()|| ans.back().back()< n.front()){
+                ans.push_back(n);
             }
             else{
-                mx= max(mx, intervals[i].back());
+                ans.back().back()= max(ans.back().back(), n.back());
             }
         }
-        ans.push_back({intervals[l].front(), mx});
         return ans;
     }
 };
