26db7cf4a821c61e1e276f0084fd8af5a281e7c2
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-08-24 18:19:04 +0530

    Time: 177 ms (95.64%), Space: 92.1 MB (34.52%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,47 +1,28 @@
 class Solution {
 public:
-    int dp[100001][3];
-    
-    bool isValid(vector<int>& nums, int i, int len){
+    int helper(vector<int> &nums, vector<int> &dp, int i){
+        if(i>=nums.size()) return true;
         
-        if(i==nums.size()){
-            if(len==0) return dp[i][len]= true;
-            if(len==1) return dp[i][len] = false;
-            else{
-                if(nums[i-1]==nums[i-2]) return dp[i][len]= true;
-                else return dp[i][len]= false;
+        if(dp[i]!=-1) return dp[i];
+        
+        if(i+1< nums.size() && nums[i+1]==nums[i]){
+            if(helper(nums,dp, i+2)) return true;
+            if(i+2<nums.size() && nums[i+2]== nums[i]){
+                if(helper(nums,dp,i+3)) return true;
             }
         }
         
-        if(dp[i][len]!=-1) return dp[i][len];
-        
-        if(len==0)
-            return dp[i][len]= isValid(nums, i+1, 1);
-        
-        if(len==1){
-            if(nums[i]==nums[i-1]){
-                return dp[i][len]= isValid(nums,i+1, 0) || isValid(nums, i+1, 2);
-            }
-            if(nums[i]-nums[i-1]==1){
-                return dp[i][len]= isValid(nums, i+1, 2);
-            }
-            else return dp[i][len]=  false;
-        }
-        
-        if(len==2){
-            if(nums[i-1]==nums[i-2]){
-                if(nums[i]==nums[i-1]) return dp[i][len]= isValid(nums, i+1,0);
-                else return dp[i][len]= false;
-            } 
-            if(nums[i-1]== 1+ nums[i-2]){
-                if(nums[i]==1+ nums[i-1]) return dp[i][len]= isValid(nums, i+1, 0);
-                else return dp[i][len]=  false;
+        else if(i+1< nums.size() && nums[i+1]==1+ nums[i]){
+            // if(helper(nums,dp, i+2)) return true;
+            if(i+2<nums.size() && nums[i+2]== 2+ nums[i]){
+                if(helper(nums,dp,i+3)) return true;
             }
         }
-        return dp[i][len] = false;
+        
+        return dp[i]= false;
     }
     bool validPartition(vector<int>& nums) {
-        memset(dp,-1, sizeof(dp));
-        return isValid(nums, 0, 0);
+        vector<int> dp(nums.size(),-1);
+        return helper(nums, dp,0);
     }
 };
