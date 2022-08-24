d9cb53d52413c29fa7040dbae9fbd0466c0ff19d
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-08-25 02:30:50 +0530

    Time: 233 ms (97.27%), Space: 83.7 MB (76.76%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
@@ -6,10 +6,7 @@ public:
         unordered_map<int, int> map;
         long long gp=0;
         for(int i=0;i<n;i++){
-            if(map.find(nums[i]-i)!= map.end()){
-                gp+= map[nums[i]-i]++;
-            }
-            else map[nums[i]-i]=1;
+            gp+= map[nums[i]-i]++;
         }
         return (1LL*(n*(n-1))/2) -gp;
     }
