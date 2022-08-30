52f493be9462fe9f293d2d68883f6e71332c58c0
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-08-30 14:51:33 +0530

    Time: 427 ms (77.05%), Space: 86.5 MB (86.75%) - LeetHub

--- 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.cpp
+++ 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    int countBits(int n){
-        int cnt =0;
-        for(int i=31; i>=0 ;i--){
-            if(n & (1<<i)) cnt++;
-        }
-        return cnt;
-    }
     long long countExcellentPairs(vector<int>& nums, int k) {
         unordered_set<int> set(nums.begin(), nums.end());
-        vector<int> numBits;
         
-        for(auto &n: set)
-            numBits.push_back(countBits(n));
-        
-        sort(numBits.begin(), numBits.end());
+        vector<int> bitfreq(32,0);
+        for(auto it: set){
+            
+            int bits= __builtin_popcount(it);
+            bitfreq[bits]++;
+            
+        }
         
         long long ans=0;
-        for(int i=0;i<numBits.size(); i++){
-            auto it =  lower_bound(numBits.begin(), numBits.end(), k-numBits[i]);
-            ans+=(numBits.end()-it);
+        for(int i=0;i<32;i++){
+            
+            
+            if(bitfreq[i]==0) continue;
+            // cout<<i<<" : "<<bitfreq[i]<<" : "<<max(0,k-i)<<"  :  ans =  ";
+            for(int j= max(0,k-i); j<32; j++){
+                ans+= (1LL *bitfreq[i]*bitfreq[j]);
+            }
+            // cout<<ans<<endl;
         }
+        
         return ans;
     }
 };
