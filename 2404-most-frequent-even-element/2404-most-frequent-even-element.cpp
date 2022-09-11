bf2c5a7eb403c585d84f1cbfbc42c66527342e0d
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-09-12 03:03:47 +0530

    Time: 155 ms (25.00%), Space: 38 MB (25.00%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
@@ -2,23 +2,19 @@ class Solution {
 public:
     int mostFrequentEven(vector<int>& nums) {
         unordered_map<int, int> map;
+        int freq=0, num=INT_MAX;
         
         for(auto &n: nums){
             if(n%2==0){
                 map[n]++;
+                
+                if(map[n]> freq || map[n]==freq && n<num){
+                    freq= map[n];
+                    num= n;
+                }
             }
         }
-        int freq=0, num=INT_MAX;
         
-        for(auto & it: map){
-            if(it.second== freq){
-                num= min(it.first, num);
-            }
-            else if(it.second> freq){
-                num= it.first;
-                freq= it.second;
-            }
-        }
         
         return (num==INT_MAX)?-1: num;
     }
