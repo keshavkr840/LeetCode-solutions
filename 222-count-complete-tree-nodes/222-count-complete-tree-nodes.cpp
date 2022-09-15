396ed7e36ab999928972066cf0fe60b9feff1cfc
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-09-15 15:11:13 +0530

    Time: 75 ms (11.46%), Space: 30.9 MB (20.47%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -9,33 +9,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+#define Node TreeNode
 class Solution {
 public:
+    int heightRight(Node* node){
+        int cnt=0;
+        while(node){
+            node=node->right;
+            cnt++;
+        }
+        return cnt;
+    }
+    int heightLeft(Node* node){
+        int cnt=0;
+        
+        while(node){
+            node=node->left;
+            cnt++;
+        }
+        return cnt;
+    }
     int countNodes(TreeNode* node) {
-        if(!node) return 0;
+        if(!node)
+            return 0;
         
         int left= heightLeft(node);
-        int right = heightRight(node);
-        
-        if(left==right)
-            return (1<<left)-1;
+        int right= heightRight(node);
+        if(left== right)
+            return pow(2,left)-1;
         else
             return 1+ countNodes(node->left)+ countNodes(node->right);
-    }
-    int heightLeft(TreeNode* node){
-        int height=0;
-        while(node){
-            node= node->left;
-            height++;
-        }
-        return height;
-    }
-    int heightRight(TreeNode* node){
-        int height=0;
-        while(node){
-            node= node->right;
-            height++;
-        }
-        return height;
     }
 };
