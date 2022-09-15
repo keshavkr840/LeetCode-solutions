2d5228974ebecde7ab3ed0a5f8e4f45c171d6e9d
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-09-15 20:21:37 +0530

    Time: 27 ms (5.36%), Space: 13 MB (62.72%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -13,52 +13,53 @@ public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
         unordered_map<Node*, Node*> parent;
         queue<Node*> q;
+        parent[root]= nullptr;
         q.push(root);
-        
         Node* start;
+        
         while(!q.empty()){
-            auto node= q.front(); q.pop();
-            if(node->val==target->val)
-                start= node;
+            auto node = q.front();
+            q.pop();
+            
+            if(node==target)
+                start= target;
             
             if(node->left){
-                parent[node->left]= node;
                 q.push(node->left);
+                parent[node->left]= node;
             }
             if(node->right){
                 parent[node->right]= node;
                 q.push(node->right);
             }
         }
-        unordered_set<int> vis;
-        q.push(start);
-        int level=0;
         
-        
-        while(!q.empty()){
-            int n =q.size();
-            if(level==k) break;
-            
-            for(int i=0;i<n;i++){
-                auto node = q.front(); q.pop();
-                vis.insert(node->val);
-                
-                if(node->left && vis.find(node->left->val)== vis.end())
-                    q.push(node->left);
-                if(node->right && vis.find(node->right->val) ==vis.end())
-                    q.push(node->right);
-                
-                if(parent.find(node)== parent.end()) continue;
-                if(vis.find(parent[node]->val)==vis.end())
-                    q.push(parent[node]);
-            }
-            level++;
-        }
         vector<int> ans;
-        while(!q.empty()){
-            ans.push_back(q.front()->val);
-            q.pop();
+        unordered_set<int> vis;
+        queue<pair<Node*, int>> qu;
+        qu.push({start,0});
+        
+        while(!qu.empty()){
+            auto [node, dis]= qu.front();
+            qu.pop();
+            if(vis.find(node->val)!= vis.end())
+                continue;
+            vis.insert(node->val);
+            
+            if(dis==k)
+                ans.push_back(node->val);
+            
+            if(node->left && dis<k){
+                qu.push({node->left,dis+1});
+            }
+            if(node->right && dis<k){
+                qu.push({node->right, dis+1});
+            }
+            if(parent[node] && dis<k){
+                qu.push({parent[node], dis+1});
+            }
         }
+        
         return ans;
     }
 };
