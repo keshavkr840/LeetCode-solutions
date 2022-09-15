0fab52feecf9e5bab6989027d48bc5560f1ebad8
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-09-15 17:09:58 +0530

    Time: 1162 ms (7.71%), Space: 172.5 MB (15.18%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> findOriginalArray(vector<int>& changed) {
+        int n = changed.size();
+        if(n%2!=0) return {};
+        
+        sort(changed.begin(), changed.end());
+        
+        multiset<int> map;
+        vector<int> ans;
+        
+        for(auto &e: changed){
+            if(e%2==0){
+                auto it = map.find(e/2);
+                if(it!=map.end()){
+                    map.erase(it);
+                    ans.push_back(e/2);
+                    continue;
+                }
+            }
+            map.insert(e);
+        }
+        
+        for(auto &e: ans)
+            cout<<e<<", ";
+        
+        if(map.size()) return {};
+        else return ans;
+    }
+};
