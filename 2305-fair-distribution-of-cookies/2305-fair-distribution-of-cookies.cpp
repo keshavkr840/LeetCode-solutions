b8037f4558a4b624197448114d641d5416b6a5cc
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-09-25 02:33:36 +0530

    Time: 1681 ms (40.54%), Space: 6.9 MB (80.24%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -1,42 +1,24 @@
 class Solution {
-public:
-    bool solve(vector<int>& cookies, vector<int> &v, int &k,int &mid, int idx){
-        if(idx==cookies.size()) return true;
-        
-        for(int i=0;i<k;i++){
-            if(v[i]+cookies[idx]<= mid){
-                v[i]+=cookies[idx];
-                if(solve(cookies, v, k, mid, idx+1)) return true;
-                v[i]-=cookies[idx];
-            }
-            if(v[i]==0) return false;
+private:
+    int mx= INT_MAX;
+    void solve(vector<int> &cookies, vector<int> &kSum, int i){
+        if(i==cookies.size()){
+            int currmx= *max_element(kSum.begin(), kSum.end());
+            mx= min(mx, currmx);
+            return;
         }
-        return false;
+        
+        for(int p=0;p<kSum.size();p++){
+            kSum[p]+=cookies[i];
+            solve(cookies, kSum,i+1);
+            kSum[p]-= cookies[i];
+        }
     }
+public:
     int distributeCookies(vector<int>& cookies, int k) {
+        vector<int> kSum(k,0);
         
-        sort(cookies.begin(), cookies.end());
-        
-        int l= INT_MIN, sum=0;
-        
-        for(auto &n :cookies){
-            sum+=n;
-            l= max(l, n);
-        }
-        vector<int> v(k);
-        int r= sum, ans= sum;
-        
-        while(l<=r){
-            fill(v.begin(), v.end(),0);
-            int mid= (l+r)>>1;
-            if(solve(cookies, v,k, mid,0)){
-                ans= mid;
-                r= mid-1;
-            }
-            else{
-                l= mid+1;
-            }
-        }
-        return ans;
+        solve(cookies, kSum, 0);
+        return mx;
     }
 };
