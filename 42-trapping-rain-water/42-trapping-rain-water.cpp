a24273454e6c305a8c6610325b3b480a55f34a41
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-09-25 01:50:11 +0530

    Time: 23 ms (72.18%), Space: 19.7 MB (96.62%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,3 +1,6 @@
+//https://www.youtube.com/watch?v=m18Hntz4go8&ab_channel=takeUforward
+//from 16:20 to 19:20
+
 class Solution {
 public:
     int trap(vector<int>& a) {
@@ -6,6 +9,7 @@ public:
         int l=0, r= a.size()-1;
         
         while(l<=r){
+            //this guarantees that on the other half, there is a building whose min height is that of current element
             if(a[l]<=a[r]){
                 if(a[l]>= mxleft)
                     mxleft= a[l++];
@@ -22,6 +26,8 @@ public:
         }
         
         return ans;
-        
+//we know that soln. is min(mxleft, mxright)-height[i]
+//so, we compare at each step whether now, the height[l] is less or right is less,
+//and we move our pointer from the side which has lesser height
     }
 };
