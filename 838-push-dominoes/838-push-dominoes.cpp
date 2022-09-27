83ca1ea75ea187a1ea272da0b62b21cff287fcf3
Keshav Singh <56981030+keshavkr840@users.noreply.github.com>
2022-09-28 00:58:39 +0530

    Time: 35 ms (94.97%), Space: 12.7 MB (94.28%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.cpp
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -1,47 +1,37 @@
 class Solution {
 public:
-    string pushDominoes(string dominoes) {
-        int n = dominoes.size();
-        vector<int> left(n,-1), right(n,-1);
-        
-        int nright= -1;
-        for(int i=0;i<n;i++){
-            if(dominoes[i]=='.'){
-                if(nright==-1) continue;
-                right[i]= i- nright;
-            }
-            else if(dominoes[i]=='R')
-                nright=i;
-            else
-                nright=-1;
-        }
-        
-        int nleft=-1;
-        for(int i=n-1;i>=0;i--){
-            if(dominoes[i]=='.'){
-                if(nleft==-1) continue;
-                left[i]= nleft- i;
-            }
-            else if(dominoes[i]=='L')
-                nleft=i;
-            else
-                nleft=-1;
-        }
+    string pushDominoes(string s) {
+        int n = s.size();
+        int l=0, r=-1;
         
         for(int i=0;i<n;i++){
-            if(dominoes[i]=='.'){
-                if(left[i]==-1 && right[i]==-1) continue;
-                else if(left[i]==-1 || right[i]==-1)
-                    dominoes[i]= (left[i]==-1)?'R': 'L';
+            
+            if(s[i]=='L'){
+                if(r==-1){
+                    int j= i-1;
+                    while(j>=0 && s[j]=='.')
+                        s[j--]='L';
+                }
                 else{
-                    if(left[i]>right[i])
-                        dominoes[i]= 'R';
-                    else if( right[i]>left[i])
-                        dominoes[i]='L';
+                    for(int j=r+1, k=i-1;j<k ; j++, k--)
+                        s[j]='R', s[k]='L';
+                    r=-1;
                 }
             }
+            else if(s[i]=='R'){
+                if(r!=-1)
+                    for(int j=r+1;j<i;j++)
+                        s[j]='R';
+                
+                r=i;
+                    
+            }
         }
         
-        return dominoes;
+        if(r!=-1){
+            for(int j=r+1; j< n; j++)
+                s[j]='R';
+        }
+        return s;
     }
 };
